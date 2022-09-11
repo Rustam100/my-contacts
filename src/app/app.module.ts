13f4c81ab25e54a7e2f8ba4fcab3ab9ad7453ff6
Джафаров Рустам <dzhafarovra@gmail.com>
2022-09-11 22:17:19 +0500

    deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgxsModule } from '@ngxs/store';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { EditContactComponent } from './components/contacts/components/edit-contact/edit-contact.component';
-import { CreateContactComponent } from './components/contacts/components/create-contact/create-contact.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ContactState } from './store/state';
-import { environment } from 'src/environments/environment';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgxsModule } from "@ngxs/store";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContactsComponent } from "./components/contacts/contacts.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { EditContactComponent } from "./components/contacts/components/edit-contact/edit-contact.component";
+import { CreateContactComponent } from "./components/contacts/components/create-contact/create-contact.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ContactState } from "./store/state";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateContactComponent,
     ContactsComponent,
-    EditContactComponent
+    EditContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxsModule.forRoot([ContactState], {
-      developmentMode: !environment.production
-    }),
+    NgxsModule.forRoot([ContactState]),
     FontAwesomeModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
